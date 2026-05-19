ecf5575eb8e6f144278023afae488a962c73c0e0
Yayufan <kamikazey2200@gmail.com>
2026-05-19 17:02:39 +0800

    update

--- conference-admin/src/main/java/tw/com/conference/strategy/project/FreeModeStrategy.java
+++ conference-admin/src/main/java/tw/com/conference/strategy/project/FreeModeStrategy.java
@@ -40,57 +40,57 @@ public class FreeModeStrategy implements ProjectModeStrategy {
 	@Override
 	public void handleRegistration(Member member) {
 
-		// 1.創建「免費」註冊費訂單，狀態為 「已付款」
-		ordersService.createFreeRegistrationOrder(member);
-
-		// 2.創建註冊成功通知信件內容
-		EmailBodyContent registrationSuccessContent = notificationService.generateRegistrationSuccessContent(member,
-				BANNER_PHOTO_URL);
-
-		// 3.異步寄送信件
-		asyncService.sendCommonEmail(member.getEmail(), PROJECT_NAME + " Registration Successful",
-				registrationSuccessContent.getHtmlContent(), registrationSuccessContent.getPlainTextContent());
-
-		// 4.新增進與會者名單
-		Attendee attendee = attendeeService.addAttendee(member);
-
-		// 5.獲取當下 Attendee 群體的Index,用於後續標籤分組
-		int attendeeGroupIndex = attendeeService.getAttendeeGroupIndex(GROUP_SIZE);
-
-		// 6.與會者標籤分組
-		// 拿到 Tag（不存在則新增Tag）
-		Tag attendeeGroupTag = tagService.getOrCreateAttendeesGroupTag(attendeeGroupIndex);
-		// 關聯 Attendee 與 Tag
-		attendeeTagService.addAttendeeTag(attendee.getAttendeeId(), attendeeGroupTag.getTagId());
+//		// 1.創建「免費」註冊費訂單，狀態為 「已付款」
+//		ordersService.createFreeRegistrationOrder(member);
+//
+//		// 2.創建註冊成功通知信件內容
+//		EmailBodyContent registrationSuccessContent = notificationService.generateRegistrationSuccessContent(member,
+//				BANNER_PHOTO_URL);
+//
+//		// 3.異步寄送信件
+//		asyncService.sendCommonEmail(member.getEmail(), PROJECT_NAME + " Registration Successful",
+//				registrationSuccessContent.getHtmlContent(), registrationSuccessContent.getPlainTextContent());
+//
+//		// 4.新增進與會者名單
+//		Attendee attendee = attendeeService.addAttendee(member);
+//
+//		// 5.獲取當下 Attendee 群體的Index,用於後續標籤分組
+//		int attendeeGroupIndex = attendeeService.getAttendeeGroupIndex(GROUP_SIZE);
+//
+//		// 6.與會者標籤分組
+//		// 拿到 Tag（不存在則新增Tag）
+//		Tag attendeeGroupTag = tagService.getOrCreateAttendeesGroupTag(attendeeGroupIndex);
+//		// 關聯 Attendee 與 Tag
+//		attendeeTagService.addAttendeeTag(attendee.getAttendeeId(), attendeeGroupTag.getTagId());
 
 	}
 
 	@Override
 	public void handleGroupRegistration(Member member, boolean isMaster, BigDecimal totalFee) {
 
-		// 1.創建 「免費」 團體註冊費訂單，狀態為 「已付款」
-		ordersService.createFreeGroupRegistrationPaidOrder(member);
-
-		// 2.產生系統團體報名通知信
-		EmailBodyContent groupRegistrationSuccessContent = notificationService
-				.generateGroupRegistrationSuccessContent(member, BANNER_PHOTO_URL);
-
-		// 3.寄信個別通知會員，團體報名成功
-		asyncService.sendCommonEmail(member.getEmail(), PROJECT_NAME + " GROUP Registration Successful",
-				groupRegistrationSuccessContent.getHtmlContent(),
-				groupRegistrationSuccessContent.getPlainTextContent());
-
-		// 4.新增進與會者名單
-		Attendee attendee = attendeeService.addAttendee(member);
-
-		// 5.獲取當下 Attendee 群體的Index,用於後續標籤分組
-		int attendeeGroupIndex = attendeeService.getAttendeeGroupIndex(GROUP_SIZE);
-
-		// 6.與會者標籤分組
-		// 拿到 Tag（不存在則新增Tag）
-		Tag attendeeGroupTag = tagService.getOrCreateAttendeesGroupTag(attendeeGroupIndex);
-		// 關聯 Attendee 與 Tag
-		attendeeTagService.addAttendeeTag(attendee.getAttendeeId(), attendeeGroupTag.getTagId());
+//		// 1.創建 「免費」 團體註冊費訂單，狀態為 「已付款」
+//		ordersService.createFreeGroupRegistrationPaidOrder(member);
+//
+//		// 2.產生系統團體報名通知信
+//		EmailBodyContent groupRegistrationSuccessContent = notificationService
+//				.generateGroupRegistrationSuccessContent(member, BANNER_PHOTO_URL);
+//
+//		// 3.寄信個別通知會員，團體報名成功
+//		asyncService.sendCommonEmail(member.getEmail(), PROJECT_NAME + " GROUP Registration Successful",
+//				groupRegistrationSuccessContent.getHtmlContent(),
+//				groupRegistrationSuccessContent.getPlainTextContent());
+//
+//		// 4.新增進與會者名單
+//		Attendee attendee = attendeeService.addAttendee(member);
+//
+//		// 5.獲取當下 Attendee 群體的Index,用於後續標籤分組
+//		int attendeeGroupIndex = attendeeService.getAttendeeGroupIndex(GROUP_SIZE);
+//
+//		// 6.與會者標籤分組
+//		// 拿到 Tag（不存在則新增Tag）
+//		Tag attendeeGroupTag = tagService.getOrCreateAttendeesGroupTag(attendeeGroupIndex);
+//		// 關聯 Attendee 與 Tag
+//		attendeeTagService.addAttendeeTag(attendee.getAttendeeId(), attendeeGroupTag.getTagId());
 
 	}
 
